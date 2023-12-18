a7d13be5d17656795594db1260d83b78aaf61afd
MonAmiral <jerome.bretheau@gmail.com>
2023-12-18 17:22:54 +0100

    Mod template

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using LC_API;
+using GameNetcodeStuff;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.Netcode;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine.Animations;
+
+namespace GiantChibi
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("LC_API")]
+    [BepInProcess("Lethal Company.exe")]
+    public class Plugin : BaseUnityPlugin
+	{
+		public static Harmony _harmony;
+
+		private void Awake()
+		{
+			_harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+			_harmony.PatchAll();
+			Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} loaded");
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+
+# You're my friend, right? Come here!
+
+![](https://static-cdn.jtvnw.net/jtv_user_pictures/80055aff-4463-4332-939d-2a5d5ff08bec-profile_image-300x300.png)
+
+## Features:
+Replaces the forest giant model with chibidoki.
+- Works in multiplayer even if the other players do not have the mod installed, you'll just be the one to see the giant chibi!
+- Adds voicelines for when she spots you or grabs you.
+
+## Credits:
+- Chibidoki is the funniest vtuber of 2023: https://linktr.ee/chibidoki
+
+## Updates:
+**[0.0.1]**  
+- 
